f0accff1cf6c2be588006a88309bb29de51caba5
kzhang <kezhang@psych.ubc.ca>
2017-10-19 10:30:37 -0700

    chisqures

--- UG_data_analysis/fairness_describ.py
+++ UG_data_analysis/fairness_describ.py
@@ -50,30 +50,30 @@ def byGroup_reappraisal_fair():
     ##baseline condition and experimental condition are all combine
 
     df = pd.read_csv('/Users/kezhang/ownCloud/Suicide_UG/UG_clean_updated/ug_all_task_data.csv', encoding = "ISO-8859-1")
-    df['ReappraisalDirection'].fillna('baseline', inplace = True)
-    df['ReappraisalDirection'] = np.where(df['ReappraisalDirection'] == 1, 'punish',
-                                          np.where(df['ReappraisalDirection'] == 2, 'empathy', df['ReappraisalDirection']))
-    df.to_csv('/Users/kezhang/ownCloud/Suicide_UG/UG_clean_updated/ug_all_task_data.csv', index=False)
+    # df['ReappraisalDirection'].fillna('baseline', inplace = True)
+    # df['ReappraisalDirection'] = np.where(df['ReappraisalDirection'] == 1, 'punish',
+    #                                       np.where(df['ReappraisalDirection'] == 2, 'empathy', df['ReappraisalDirection']))
+    # df.to_csv('/Users/kezhang/ownCloud/Suicide_UG/UG_clean_updated/ug_all_task_data.csv', index=False)
 
     ct_group5_reappraisal = pd.crosstab([df.group5, df.ReappraisalDirection, df.fairness], df.AcceptOffer, normalize='index')
     ct_group4_reappraisal = pd.crosstab([df.group4, df.ReappraisalDirection, df.fairness], df.AcceptOffer, normalize='index')
 
-    ct_group5 = pd.crosstab([df.group5, df.fairness], df.AcceptOffer, normalize='index')
-    ct_group4 = pd.crosstab([df.group4, df.fairness], df.AcceptOffer, normalize='index')
-
-    workbook = xlsxwriter.Workbook('/Users/kezhang/ownCloud/Suicide_UG/UG_clean_updated/fair_reappraisal.xlsx')
-    workbook.close()
+    # ct_group5 = pd.crosstab([df.group5, df.fairness], df.AcceptOffer, normalize='index')
+    # ct_group4 = pd.crosstab([df.group4, df.fairness], df.AcceptOffer, normalize='index')
 
     book = load_workbook('/Users/kezhang/ownCloud/Suicide_UG/UG_clean_updated/fair_reappraisal.xlsx')
     writer = pd.ExcelWriter('/Users/kezhang/ownCloud/Suicide_UG/UG_clean_updated/fair_reappraisal.xlsx', engine='openpyxl')
     writer.book = book
+    writer.sheets = dict((ws.title, ws) for ws in book.worksheets)
 
-    ct_group5_reappraisal.to_excel(writer, 'group5_reappraisal')
-    ct_group4_reappraisal.to_excel(writer, 'group4_reappraisal')
-    ct_group5.to_excel(writer, 'group5')
-    ct_group4.to_excel(writer, 'group4')
+    ct_group5_reappraisal.to_excel(writer, 'group5_reappraisal_percent')
+    ct_group4_reappraisal.to_excel(writer, 'group4_reappraisal_percent')
+    # ct_group5.to_excel(writer, 'group5')
+    # ct_group4.to_excel(writer, 'group4')
     writer.save()
     writer.close()
+
+byGroup_reappraisal_fair()
 
 def fairness():
     #check the main effect of fairness, whether people accept more fair offer in terms of percentage.
@@ -89,39 +89,55 @@ def byGroup_baseline_fairness():
     # only have Baseline condition
     # crosstab between fairness and groups
     # two-way chi-square
-    all_data = pd.read_csv('C:\\Users\\ke\\ownCloud\\Suicide_UG\\UG_clean_updated\\ug_all_task_data.csv', encoding="ISO-8859-1")
+    all_data = pd.read_csv('/Users/kezhang/ownCloud/Suicide_UG/UG_clean_updated/ug_all_task_data.csv', encoding="ISO-8859-1")
     baseline = all_data[all_data.ReappraisalDirection == 'baseline']
 
-    ct_group5 = pd.crosstab(baseline.group5, [baseline.fairness, baseline.AcceptOffer])
-    ct_group4 = pd.crosstab(baseline.group4, [baseline.fairness, baseline.AcceptOffer])
+    ct_group5 = pd.crosstab([baseline.group5, baseline.fairness], baseline.AcceptOffer)
+    ct_group4 = pd.crosstab([baseline.group4, baseline.fairness], baseline.AcceptOffer)
 
-    ct_group5_p = ct_group5/ct_group5.groupby(level = 0, axis=1).sum()
-    ct_group4_p = ct_group4/ct_group4.groupby(level = 0, axis=1).sum()
+    # ct_group5_p = ct_group5/ct_group5.groupby(level = 0, axis=1).sum()
+    # ct_group4_p = ct_group4/ct_group4.groupby(level = 0, axis=1).sum()
 
-    workbook = xlsxwriter.Workbook('C:\\Users\\ke\\ownCloud\\Suicide_UG\\UG_clean_updated\\chisquare_crosstab.xlsx')
-    workbook.close()
+    ct_group5_p = pd.crosstab([baseline.group5, baseline.fairness], baseline.AcceptOffer, normalize='index')
+    ct_group4_p = pd.crosstab([baseline.group4, baseline.fairness], baseline.AcceptOffer, normalize='index')
 
-    book = load_workbook('C:\\Users\\ke\\ownCloud\\Suicide_UG\\UG_clean_updated\\chisquare_crosstab.xlsx')
-    writer = pd.ExcelWriter('C:\\Users\\ke\\ownCloud\\Suicide_UG\\UG_clean_updated\\chisquare_crosstab.xlsx',
+    book = load_workbook('/Users/kezhang/ownCloud/Suicide_UG/UG_clean_updated/chisquare_crosstab.xlsx')
+    writer = pd.ExcelWriter('/Users/kezhang/ownCloud/Suicide_UG/UG_clean_updated/chisquare_crosstab.xlsx',
                             engine='openpyxl')
     writer.book = book
+    writer.sheets = dict((ws.title, ws) for ws in book.worksheets)
 
+    ct_group5.to_excel(writer, "baseline_group4_count")
+    ct_group4.to_excel(writer, "baseline_group5_count")
     ct_group4_p.to_excel(writer, 'baseline_group4')
     ct_group5_p.to_excel(writer, 'baseline_group5')
     writer.save()
-    writer.close()
+
 
 def byGroup_reappraisal_fairness():
-    all_data = pd.read_csv('C:\\Users\\ke\\ownCloud\\Suicide_UG\\UG_clean_updated\\ug_all_task_data.csv', encoding="ISO-8859-1")
+    all_data = pd.read_csv('/Users/kezhang/ownCloud/Suicide_UG/UG_clean_updated/ug_all_task_data.csv', encoding="ISO-8859-1")
     reappra = all_data[all_data.ReappraisalDirection != 'baseline']
 
-    ct_group5 = pd.crosstab([reappra.group5, reappra.fairness, reappra.ReappraisalDirection], reappra.AcceptOffer)
-    ct_group4 = pd.crosstab([reappra.group4, reappra.fairness, reappra.ReappraisalDirection], reappra.AcceptOffer)
+    ct_group5 = pd.crosstab(reappra.group5, [reappra.fairness, reappra.ReappraisalDirection, reappra.AcceptOffer], margins=True)
+    ct_group4 = pd.crosstab(reappra.group4, [reappra.fairness, reappra.ReappraisalDirection, reappra.AcceptOffer], margins=True)
+    # ct_group5_p = pd.crosstab([reappra.group5, reappra.fairness, reappra.ReappraisalDirection], reappra.AcceptOffer, normalize='index')
+    # ct_group4_p = pd.crosstab([reappra.group4, reappra.fairness, reappra.ReappraisalDirection], reappra.AcceptOffer, normalize='index')
 
-    print(ct_group5)
 
+    book = load_workbook('/Users/kezhang/ownCloud/Suicide_UG/UG_clean_updated/chisquare_crosstab.xlsx')
+    writer = pd.ExcelWriter('/Users/kezhang/ownCloud/Suicide_UG/UG_clean_updated/chisquare_crosstab.xlsx',
+                            engine='openpyxl')
+    writer.book = book
+    writer.sheets = dict((ws.title, ws) for ws in book.worksheets)
+
+    ct_group4.to_excel(writer, 'group4_3way_count')
+    # ct_group5.to_excel(writer, 'group5_3way_graph')
+    # ct_group5_p.to_excel(writer, 'group5_3way')
+    # ct_group4_p.to_excel(writer, 'group4_3way')
+    writer.save()
 
 byGroup_reappraisal_fairness()
+
 def tryversion():
     df = pd.read_excel('C:\\Users\\ke\\ownCloud\\Suicide_UG\\UG_clean_updated\\try.xlsx')
     ct = pd.crosstab(df.group, [df.position, df.offer], margins=True)
